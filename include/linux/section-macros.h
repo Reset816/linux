a3e3212a3b25bccd8f3a4e5bfc6cc4cd153c1c2b
Yuan Tan <kentertan12138@outlook.com>
2025-09-28 06:19:49 +0800

    Add section macros for relocation support
    Introduce a new header file 'section-macros.h' to define macros for handling section relocations in assembly. Update the RISC-V assembly code to utilize the new PUSHSECTION macro for cleaner and more maintainable code. Add a Kconfig option to check for assembler support of the '.reloc' directive with 'BFD_RELOC_NONE'.

---
+++ include/linux/section-macros.h
@@ -0,0 +1,46 @@
+/* SPDX-License-Identifier: GPL-2.0 */
+#ifndef _LINUX_SECTION_MACROS_H
+#define _LINUX_SECTION_MACROS_H
+
+#include <linux/stringify.h>
+#include <linux/compiler_types.h>
+
+#ifdef __ASSEMBLY__
+
+
+#ifdef CONFIG_AS_HAS_RELOC
+# define __ASM_BFD_RELOC_NONE(lbl)  .reloc ., BFD_RELOC_NONE, lbl;
+#else
+# define __ASM_BFD_RELOC_NONE(lbl) /* no-op */
+#endif
+
+#define _PUSHSECTION(lbl, sec, ...)                                             \
+	__ASM_BFD_RELOC_NONE(lbl)                                                   \
+	.pushsection sec, __VA_ARGS__ ;                                             \
+	lbl:
+
+#define PUSHSECTION(sec, ...)                                                   \
+	_PUSHSECTION(__SECTION_NAME(sec), __SECTION_NAME(sec), __VA_ARGS__)
+
+
+#else /* !__ASSEMBLY__ */
+
+#define __ASM_UNIQUE_LBL(kind)   ".L" __stringify(__UNIQUE_ID(kind))
+
+#ifdef CONFIG_AS_HAS_RELOC
+# define __ASM_BFD_RELOC_NONE(lbl)  ".reloc ., BFD_RELOC_NONE, " lbl "\n\t"
+#else
+# define __ASM_BFD_RELOC_NONE(lbl)  /* no-op */
+#endif
+
+#define _PUSHSECTION(lbl, sec, ...)                                             \
+	__ASM_BFD_RELOC_NONE(lbl)                                                   \
+	".pushsection " __stringify(sec) ", " #__VA_ARGS__ "\n\t"                   \
+	lbl ":\n\t"
+
+#define PUSHSECTION(sec, ...)                                                   \
+	_PUSHSECTION(__SECTION_NAME(sec), __SECTION_NAME(sec), __VA_ARGS__)
+
+#endif /* __ASSEMBLY__ */
+
+#endif /* _LINUX_SECTION_MACROS_H */
