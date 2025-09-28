a2e17adcd75d43d806b23a16c98388f90e2c7fcd
Yuan Tan <kentertan12138@outlook.com>
2025-09-29 07:34:12 +0800

    把.S中的PUSHSECTION换成汇编宏实现
    PUSHSECTION可能展开到.macro里面，C语言宏无法让section name unique
    未进行完整测试

--- include/linux/section-macros.h
+++ include/linux/section-macros.h
@@ -7,20 +7,18 @@
 
 #ifdef __ASSEMBLY__
 
-
+.macro  PUSHSECTION_NAMED label:req, section:req, args:vararg
 #ifdef CONFIG_AS_HAS_RELOC
-# define __ASM_BFD_RELOC_NONE(lbl)  .reloc ., BFD_RELOC_NONE, lbl;
-#else
-# define __ASM_BFD_RELOC_NONE(lbl) /* no-op */
+        .reloc  ., BFD_RELOC_NONE, \label
 #endif
+        /* 把 \section 和 .\@ 在 GAS 里直接拼成：<基名>.<唯一号> */
+        .pushsection \section\().\@, \args
+\label:
+        .endm
 
-#define _PUSHSECTION(lbl, sec, ...)                                             \
-	__ASM_BFD_RELOC_NONE(lbl)                                                   \
-	.pushsection sec, __VA_ARGS__ ;                                             \
-	lbl:
-
-#define PUSHSECTION(sec, ...)                                                   \
-	_PUSHSECTION(__SECTION_NAME(sec), __SECTION_NAME(sec), __VA_ARGS__)
+        .macro  PUSHSECTION section:req, args:vararg
+        PUSHSECTION_NAMED .Lpushsec\@, \section, \args
+        .endm
 
 
 #else /* !__ASSEMBLY__ */
@@ -39,7 +37,7 @@
 	lbl ":\n\t"
 
 #define PUSHSECTION(sec, ...)                                                   \
-	_PUSHSECTION(__SECTION_NAME(sec%=), __SECTION_NAME(sec), __VA_ARGS__)
+	_PUSHSECTION(__SECTION_NAME(sec%=), __SECTION_NAME(sec.%=), __VA_ARGS__)
 
 #endif /* __ASSEMBLY__ */
 
