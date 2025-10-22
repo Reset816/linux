67f15e8b0dd9033d16cb56be33126de5df438783
Yuan Tan <ytan089@ucr.edu>
2025-10-22 02:47:02 -0700

    Replace arch_change_bit in line 172 of arch/x86/include/asm/bitops.h

--- arch/x86/include/asm/bitops.h
+++ arch/x86/include/asm/bitops.h
@@ -169,16 +169,25 @@ static __always_inline void arch___change_bit(unsigned long nr,
 	*p = val;
 }
 
-static __always_inline void
-arch_change_bit(long nr, volatile unsigned long *addr)
+static __always_inline void arch_change_bit(long nr,
+					    volatile unsigned long *addr)
 {
 	if (__builtin_constant_p(nr)) {
-		asm volatile(LOCK_PREFIX "xorb %b1,%0"
-			: CONST_MASK_ADDR(nr, addr)
-			: "iq" (CONST_MASK(nr)));
+		unsigned long bit = (unsigned long)nr;
+		unsigned long word_index = bit / BITS_PER_LONG;
+		unsigned long mask = 1UL << (bit & (BITS_PER_LONG - 1));
+		volatile unsigned long *p = addr + word_index;
+		unsigned long value = *p;
+		value ^= mask;
+		*p = value;
 	} else {
-		asm volatile(LOCK_PREFIX __ASM_SIZE(btc) " %1,%0"
-			: : RLONG_ADDR(addr), "Ir" (nr) : "memory");
+		unsigned long bit = (unsigned long)nr;
+		unsigned long word_index = bit / BITS_PER_LONG;
+		unsigned long mask = 1UL << (bit & (BITS_PER_LONG - 1));
+		volatile unsigned long *p = addr + word_index;
+		unsigned long value = *p;
+		value ^= mask;
+		*p = value;
 	}
 }
 
