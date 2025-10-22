f848a4cf7ee3a8ecddb12d47906ffdca9d66d7d8
Yuan Tan <ytan089@ucr.edu>
2025-10-22 03:15:03 -0700

    Replace fls in line 453 of arch/x86/include/asm/bitops.h

--- arch/x86/include/asm/bitops.h
+++ arch/x86/include/asm/bitops.h
@@ -478,18 +478,26 @@ static __always_inline int fls(unsigned int x)
 	 * We cannot do this on 32 bits because at the very least some
 	 * 486 CPUs did not behave this way.
 	 */
-	asm("bsrl %1,%0"
-	    : "=r" (r)
-	    : "rm" (x), "0" (-1));
+	if (x) {
+		int shift = (int)(sizeof(unsigned int) * 8) - 1;
+		r = shift - __builtin_clz(x);
+	} else {
+		r = -1;
+	}
 #elif defined(CONFIG_X86_CMOV)
-	asm("bsrl %1,%0\n\t"
-	    "cmovzl %2,%0"
-	    : "=&r" (r) : "rm" (x), "rm" (-1));
+	if (x) {
+		int shift = (int)(sizeof(unsigned int) * 8) - 1;
+		r = shift - __builtin_clz(x);
+	} else {
+		r = -1;
+	}
 #else
-	asm("bsrl %1,%0\n\t"
-	    "jnz 1f\n\t"
-	    "movl $-1,%0\n"
-	    "1:" : "=r" (r) : "rm" (x));
+	if (x) {
+		int shift = (int)(sizeof(unsigned int) * 8) - 1;
+		r = shift - __builtin_clz(x);
+	} else {
+		r = -1;
+	}
 #endif
 	return r + 1;
 }
