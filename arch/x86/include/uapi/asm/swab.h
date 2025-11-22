be70bcf00ba6e73da25f28b96e79c99839f6d28a
Yuan Tan <ytan089@ucr.edu>
2025-11-21 23:09:13 -0800

    Replace __arch_swab64 in line 18 of arch/x86/include/uapi/asm/swab.h

--- arch/x86/include/uapi/asm/swab.h
+++ arch/x86/include/uapi/asm/swab.h
@@ -25,13 +25,32 @@ static inline __attribute_const__ __u64 __arch_swab64(__u64 val)
 		} s;
 		__u64 u;
 	} v;
+	__u32 orig_a;
+	__u32 orig_b;
+	__u32 swapped_a;
+	__u32 swapped_b;
+
 	v.u = val;
-	asm("bswapl %0 ; bswapl %1 ; xchgl %0,%1"
-	    : "=r" (v.s.a), "=r" (v.s.b)
-	    : "0" (v.s.a), "1" (v.s.b));
+	orig_a = v.s.a;
+	orig_b = v.s.b;
+	swapped_a =
+		((orig_a & 0x000000ffU) << 24) | ((orig_a & 0x0000ff00U) << 8) |
+		((orig_a & 0x00ff0000U) >> 8) | ((orig_a & 0xff000000U) >> 24);
+	swapped_b =
+		((orig_b & 0x000000ffU) << 24) | ((orig_b & 0x0000ff00U) << 8) |
+		((orig_b & 0x00ff0000U) >> 8) | ((orig_b & 0xff000000U) >> 24);
+	v.s.a = swapped_b;
+	v.s.b = swapped_a;
 	return v.u;
 #else /* __i386__ */
-	asm("bswapq %0" : "=r" (val) : "0" (val));
+	val = ((val & 0x00000000000000ffULL) << 56) |
+	      ((val & 0x000000000000ff00ULL) << 40) |
+	      ((val & 0x0000000000ff0000ULL) << 24) |
+	      ((val & 0x00000000ff000000ULL) << 8) |
+	      ((val & 0x000000ff00000000ULL) >> 8) |
+	      ((val & 0x0000ff0000000000ULL) >> 24) |
+	      ((val & 0x00ff000000000000ULL) >> 40) |
+	      ((val & 0xff00000000000000ULL) >> 56);
 	return val;
 #endif
 }
