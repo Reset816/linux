98678c23612ee1d1309297e0a9cd2b5b36369811
Yuan Tan <ytan089@ucr.edu>
2025-11-21 23:02:08 -0800

    Replace __arch_hweight32 in line 15 of arch/x86/include/asm/arch_hweight.h

--- arch/x86/include/asm/arch_hweight.h
+++ arch/x86/include/asm/arch_hweight.h
@@ -16,9 +16,7 @@ static __always_inline unsigned int __arch_hweight32(unsigned int w)
 {
 	unsigned int res;
 
-	asm (ALTERNATIVE("call __sw_hweight32", "popcntl %1, %0", X86_FEATURE_POPCNT)
-			 : "="REG_OUT (res)
-			 : REG_IN (w));
+	res = __sw_hweight32(w);
 
 	return res;
 }
