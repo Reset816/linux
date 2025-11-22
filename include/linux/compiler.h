b6521cd20ec627caa929246dca9ee0bf75342435
Yuan Tan <ytan089@ucr.edu>
2025-11-21 22:36:46 -0800

    Replace anonymous in line 1 of include/linux/compiler.h

--- include/linux/compiler.h
+++ include/linux/compiler.h
@@ -12,71 +12,68 @@
  * Note: DISABLE_BRANCH_PROFILING can be used by special lowlevel code
  * to disable branch tracing on a per file basis.
  */
-void ftrace_likely_update(struct ftrace_likely_data *f, int val,
-			  int expect, int is_constant);
-#if defined(CONFIG_TRACE_BRANCH_PROFILING) \
-    && !defined(DISABLE_BRANCH_PROFILING) && !defined(__CHECKER__)
-#define likely_notrace(x)	__builtin_expect(!!(x), 1)
-#define unlikely_notrace(x)	__builtin_expect(!!(x), 0)
+void ftrace_likely_update(struct ftrace_likely_data *f, int val, int expect,
+			  int is_constant);
+#if defined(CONFIG_TRACE_BRANCH_PROFILING) && \
+	!defined(DISABLE_BRANCH_PROFILING) && !defined(__CHECKER__)
+#define likely_notrace(x) __builtin_expect(!!(x), 1)
+#define unlikely_notrace(x) __builtin_expect(!!(x), 0)
 
-#define __branch_check__(x, expect, is_constant) ({			\
-			long ______r;					\
-			static struct ftrace_likely_data		\
-				__aligned(4)				\
-				__section("_ftrace_annotated_branch")	\
-				______f = {				\
-				.data.func = __func__,			\
-				.data.file = __FILE__,			\
-				.data.line = __LINE__,			\
-			};						\
-			______r = __builtin_expect(!!(x), expect);	\
-			ftrace_likely_update(&______f, ______r,		\
-					     expect, is_constant);	\
-			______r;					\
-		})
+#define __branch_check__(x, expect, is_constant)                              \
+	({                                                                    \
+		long ______r;                                                 \
+		static struct ftrace_likely_data __aligned(4)                 \
+			__section("_ftrace_annotated_branch") ______f = {     \
+				.data.func = __func__,                        \
+				.data.file = __FILE__,                        \
+				.data.line = __LINE__,                        \
+			};                                                    \
+		______r = __builtin_expect(!!(x), expect);                    \
+		ftrace_likely_update(&______f, ______r, expect, is_constant); \
+		______r;                                                      \
+	})
 
 /*
  * Using __builtin_constant_p(x) to ignore cases where the return
  * value is always the same.  This idea is taken from a similar patch
  * written by Daniel Walker.
  */
-# ifndef likely
-#  define likely(x)	(__branch_check__(x, 1, __builtin_constant_p(x)))
-# endif
-# ifndef unlikely
-#  define unlikely(x)	(__branch_check__(x, 0, __builtin_constant_p(x)))
-# endif
+#ifndef likely
+#define likely(x) (__branch_check__(x, 1, __builtin_constant_p(x)))
+#endif
+#ifndef unlikely
+#define unlikely(x) (__branch_check__(x, 0, __builtin_constant_p(x)))
+#endif
 
 #ifdef CONFIG_PROFILE_ALL_BRANCHES
 /*
  * "Define 'is'", Bill Clinton
  * "Define 'if'", Steven Rostedt
  */
-#define if(cond, ...) if ( __trace_if_var( !!(cond , ## __VA_ARGS__) ) )
+#define if (cond, ...) if (__trace_if_var(!!(cond, ##__VA_ARGS__)))
 
-#define __trace_if_var(cond) (__builtin_constant_p(cond) ? (cond) : __trace_if_value(cond))
+#define __trace_if_var(cond) \
+	(__builtin_constant_p(cond) ? (cond) : __trace_if_value(cond))
 
-#define __trace_if_value(cond) ({			\
-	static struct ftrace_branch_data		\
-		__aligned(4)				\
-		__section("_ftrace_branch")		\
-		__if_trace = {				\
-			.func = __func__,		\
-			.file = __FILE__,		\
-			.line = __LINE__,		\
-		};					\
-	(cond) ?					\
-		(__if_trace.miss_hit[1]++,1) :		\
-		(__if_trace.miss_hit[0]++,0);		\
-})
+#define __trace_if_value(cond)                                     \
+	({                                                         \
+		static struct ftrace_branch_data __aligned(4)      \
+			__section("_ftrace_branch") __if_trace = { \
+				.func = __func__,                  \
+				.file = __FILE__,                  \
+				.line = __LINE__,                  \
+			};                                         \
+		(cond) ? (__if_trace.miss_hit[1]++, 1) :           \
+			 (__if_trace.miss_hit[0]++, 0);            \
+	})
 
 #endif /* CONFIG_PROFILE_ALL_BRANCHES */
 
 #else
-# define likely(x)	__builtin_expect(!!(x), 1)
-# define unlikely(x)	__builtin_expect(!!(x), 0)
-# define likely_notrace(x)	likely(x)
-# define unlikely_notrace(x)	unlikely(x)
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#define likely_notrace(x) likely(x)
+#define unlikely_notrace(x) unlikely(x)
 #endif
 
 /* Optimization barrier */
@@ -110,7 +107,9 @@ void ftrace_likely_update(struct ftrace_likely_data *f, int val,
 
 /* workaround for GCC PR82365 if needed */
 #ifndef barrier_before_unreachable
-# define barrier_before_unreachable() do { } while (0)
+#define barrier_before_unreachable() \
+	do {                         \
+	} while (0)
 #endif
 
 /* Unreachable code */
@@ -138,10 +137,11 @@ void ftrace_likely_update(struct ftrace_likely_data *f, int val,
 #endif /* CONFIG_OBJTOOL */
 
 #ifndef unreachable
-# define unreachable() do {		\
-	annotate_unreachable();		\
-	__builtin_unreachable();	\
-} while (0)
+#define unreachable()                    \
+	do {                             \
+		annotate_unreachable();  \
+		__builtin_unreachable(); \
+	} while (0)
 #endif
 
 /*
@@ -159,27 +159,31 @@ void ftrace_likely_update(struct ftrace_likely_data *f, int val,
  * boot/exception vector code rather than annotate each function and data.
  */
 #ifndef KENTRY
-# define KENTRY(sym)						\
-	extern typeof(sym) sym;					\
-	static const unsigned long __kentry_##sym		\
-	__used							\
-	__attribute__((__section__("___kentry+" #sym)))		\
-	= (unsigned long)&sym;
+#define KENTRY(sym)                                               \
+	extern typeof(sym) sym;                                   \
+	static const unsigned long __kentry_##sym __used          \
+		__attribute__((__section__("___kentry+" #sym))) = \
+			(unsigned long)&sym;
 #endif
 
 #ifndef RELOC_HIDE
-# define RELOC_HIDE(ptr, off)					\
-  ({ unsigned long __ptr;					\
-     __ptr = (unsigned long) (ptr);				\
-    (typeof(ptr)) (__ptr + (off)); })
+#define RELOC_HIDE(ptr, off)                  \
+	({                                    \
+		unsigned long __ptr;          \
+		__ptr = (unsigned long)(ptr); \
+		(typeof(ptr))(__ptr + (off)); \
+	})
 #endif
 
-#define absolute_pointer(val)	RELOC_HIDE((void *)(val), 0)
+#define absolute_pointer(val) RELOC_HIDE((void *)(val), 0)
 
 #ifndef OPTIMIZER_HIDE_VAR
 /* Make the optimizer believe the variable can be manipulated arbitrarily. */
-#define OPTIMIZER_HIDE_VAR(var)						\
-	__asm__ ("" : "=r" (var) : "0" (var))
+#define OPTIMIZER_HIDE_VAR(var)                           \
+	do {                                              \
+		volatile typeof(var) *__opt_var = &(var); \
+		__opt_var[0] = __opt_var[0];              \
+	} while (0)
 #endif
 
 #define __UNIQUE_ID(prefix) __PASTE(__PASTE(__UNIQUE_ID_, prefix), __COUNTER__)
@@ -194,15 +198,15 @@ void ftrace_likely_update(struct ftrace_likely_data *f, int val,
  * This macro *does not* affect normal code generation, but is a hint
  * to tooling that data races here are to be ignored.
  */
-#define data_race(expr)							\
-({									\
-	__unqual_scalar_typeof(({ expr; })) __v = ({			\
-		__kcsan_disable_current();				\
-		expr;							\
-	});								\
-	__kcsan_enable_current();					\
-	__v;								\
-})
+#define data_race(expr)                                      \
+	({                                                   \
+		__unqual_scalar_typeof(({ expr; })) __v = ({ \
+			__kcsan_disable_current();           \
+			expr;                                \
+		});                                          \
+		__kcsan_enable_current();                    \
+		__v;                                         \
+	})
 
 #endif /* __KERNEL__ */
 
