9063c8d23f967033b07b4e39b7a72d532724a43d
Yuan Tan <ytan089@ucr.edu>
2025-11-22 00:57:27 -0800

    Replace __iter_div_u64_rem in line 5 of include/vdso/math64.h

--- include/vdso/math64.h
+++ include/vdso/math64.h
@@ -2,15 +2,14 @@
 #ifndef __VDSO_MATH64_H
 #define __VDSO_MATH64_H
 
-static __always_inline u32
-__iter_div_u64_rem(u64 dividend, u32 divisor, u64 *remainder)
+static __always_inline u32 __iter_div_u64_rem(u64 dividend, u32 divisor,
+					      u64 *remainder)
 {
 	u32 ret = 0;
 
 	while (dividend >= divisor) {
-		/* The following asm() prevents the compiler from
-		   optimising this loop into a modulo operation.  */
-		asm("" : "+rm"(dividend));
+		volatile u64 tmp = dividend;
+		dividend = tmp;
 
 		dividend -= divisor;
 		ret++;
