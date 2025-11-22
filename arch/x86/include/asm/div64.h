8d23dcfdadc64ec45948f703f1867d41b2305aea
Yuan Tan <ytan089@ucr.edu>
2025-11-21 23:11:56 -0800

    Replace do_div in line 22 of arch/x86/include/asm/div64.h

--- arch/x86/include/asm/div64.h
+++ arch/x86/include/asm/div64.h
@@ -19,26 +19,35 @@
  * This ends up being the most efficient "calling
  * convention" on x86.
  */
-#define do_div(n, base)						\
-({								\
-	unsigned long __upper, __low, __high, __mod, __base;	\
-	__base = (base);					\
-	if (__builtin_constant_p(__base) && is_power_of_2(__base)) { \
-		__mod = n & (__base - 1);			\
-		n >>= ilog2(__base);				\
-	} else {						\
-		asm("" : "=a" (__low), "=d" (__high) : "A" (n));\
-		__upper = __high;				\
-		if (__high) {					\
-			__upper = __high % (__base);		\
-			__high = __high / (__base);		\
-		}						\
-		asm("divl %2" : "=a" (__low), "=d" (__mod)	\
-			: "rm" (__base), "0" (__low), "1" (__upper));	\
-		asm("" : "=A" (n) : "a" (__low), "d" (__high));	\
-	}							\
-	__mod;							\
-})
+#define do_div(n, base)                                                        \
+	({                                                                     \
+		unsigned long __upper, __low, __high, __mod, __base;           \
+		__base = (base);                                               \
+		if (__builtin_constant_p(__base) && is_power_of_2(__base)) {   \
+			__mod = n & (__base - 1);                              \
+			n >>= ilog2(__base);                                   \
+		} else {                                                       \
+			const unsigned int __word_bits =                       \
+				sizeof(unsigned long) * 8;                     \
+			unsigned long long __combined;                         \
+			__low = (unsigned long)n;                              \
+			__high = (unsigned long)(((unsigned long long)n) >>    \
+						 __word_bits);                 \
+			__upper = __high;                                      \
+			if (__high) {                                          \
+				__upper = __high % (__base);                   \
+				__high = __high / (__base);                    \
+			}                                                      \
+			__combined =                                           \
+				((unsigned long long)__upper << __word_bits) | \
+				__low;                                         \
+			__mod = (unsigned long)(__combined % __base);          \
+			__low = (unsigned long)(__combined / __base);          \
+			n = (((unsigned long long)__high) << __word_bits) |    \
+			    __low;                                             \
+		}                                                              \
+		__mod;                                                         \
+	})
 
 static inline u64 div_u64_rem(u64 dividend, u32 divisor, u32 *remainder)
 {
