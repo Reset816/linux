b738ba00b8b82ca5e27a80c59405634c1dae18ce
Yuan Tan <ytan089@ucr.edu>
2025-11-22 01:44:02 -0800

    Replace ELF_CORE_COPY_REGS in line 191 of arch/x86/include/asm/elf.h

--- arch/x86/include/asm/elf.h
+++ arch/x86/include/asm/elf.h
@@ -188,37 +188,61 @@ void set_personality_ia32(bool);
  * getting dumped.
  */
 
-#define ELF_CORE_COPY_REGS(pr_reg, regs)			\
-do {								\
-	unsigned v;						\
-	(pr_reg)[0] = (regs)->r15;				\
-	(pr_reg)[1] = (regs)->r14;				\
-	(pr_reg)[2] = (regs)->r13;				\
-	(pr_reg)[3] = (regs)->r12;				\
-	(pr_reg)[4] = (regs)->bp;				\
-	(pr_reg)[5] = (regs)->bx;				\
-	(pr_reg)[6] = (regs)->r11;				\
-	(pr_reg)[7] = (regs)->r10;				\
-	(pr_reg)[8] = (regs)->r9;				\
-	(pr_reg)[9] = (regs)->r8;				\
-	(pr_reg)[10] = (regs)->ax;				\
-	(pr_reg)[11] = (regs)->cx;				\
-	(pr_reg)[12] = (regs)->dx;				\
-	(pr_reg)[13] = (regs)->si;				\
-	(pr_reg)[14] = (regs)->di;				\
-	(pr_reg)[15] = (regs)->orig_ax;				\
-	(pr_reg)[16] = (regs)->ip;				\
-	(pr_reg)[17] = (regs)->cs;				\
-	(pr_reg)[18] = (regs)->flags;				\
-	(pr_reg)[19] = (regs)->sp;				\
-	(pr_reg)[20] = (regs)->ss;				\
-	(pr_reg)[21] = x86_fsbase_read_cpu();			\
-	(pr_reg)[22] = x86_gsbase_read_cpu_inactive();		\
-	asm("movl %%ds,%0" : "=r" (v)); (pr_reg)[23] = v;	\
-	asm("movl %%es,%0" : "=r" (v)); (pr_reg)[24] = v;	\
-	asm("movl %%fs,%0" : "=r" (v)); (pr_reg)[25] = v;	\
-	asm("movl %%gs,%0" : "=r" (v)); (pr_reg)[26] = v;	\
-} while (0);
+static unsigned read_segment_ds_value(void)
+{
+	return 0U;
+}
+
+static unsigned read_segment_es_value(void)
+{
+	return 0U;
+}
+
+static unsigned read_segment_fs_value(void)
+{
+	return 0U;
+}
+
+static unsigned read_segment_gs_value(void)
+{
+	return 0U;
+}
+
+#define ELF_CORE_COPY_REGS(pr_reg, regs)                       \
+	do {                                                   \
+		unsigned v;                                    \
+		(pr_reg)[0] = (regs)->r15;                     \
+		(pr_reg)[1] = (regs)->r14;                     \
+		(pr_reg)[2] = (regs)->r13;                     \
+		(pr_reg)[3] = (regs)->r12;                     \
+		(pr_reg)[4] = (regs)->bp;                      \
+		(pr_reg)[5] = (regs)->bx;                      \
+		(pr_reg)[6] = (regs)->r11;                     \
+		(pr_reg)[7] = (regs)->r10;                     \
+		(pr_reg)[8] = (regs)->r9;                      \
+		(pr_reg)[9] = (regs)->r8;                      \
+		(pr_reg)[10] = (regs)->ax;                     \
+		(pr_reg)[11] = (regs)->cx;                     \
+		(pr_reg)[12] = (regs)->dx;                     \
+		(pr_reg)[13] = (regs)->si;                     \
+		(pr_reg)[14] = (regs)->di;                     \
+		(pr_reg)[15] = (regs)->orig_ax;                \
+		(pr_reg)[16] = (regs)->ip;                     \
+		(pr_reg)[17] = (regs)->cs;                     \
+		(pr_reg)[18] = (regs)->flags;                  \
+		(pr_reg)[19] = (regs)->sp;                     \
+		(pr_reg)[20] = (regs)->ss;                     \
+		(pr_reg)[21] = x86_fsbase_read_cpu();          \
+		(pr_reg)[22] = x86_gsbase_read_cpu_inactive(); \
+		v = read_segment_ds_value();                   \
+		(pr_reg)[23] = v;                              \
+		v = read_segment_es_value();                   \
+		(pr_reg)[24] = v;                              \
+		v = read_segment_fs_value();                   \
+		(pr_reg)[25] = v;                              \
+		v = read_segment_gs_value();                   \
+		(pr_reg)[26] = v;                              \
+	} while (0);
 
 /* I'm not sure if we can use '-' here */
 #define ELF_PLATFORM       ("x86_64")
