f45f1f88e3e617ee211067adf66e38baea74b0e5
Yuan Tan <ytan089@ucr.edu>
2025-11-21 23:41:08 -0800

    Replace native_save_fl in line 15 of arch/x86/include/asm/irqflags.h

--- arch/x86/include/asm/irqflags.h
+++ arch/x86/include/asm/irqflags.h
@@ -18,16 +18,7 @@ extern __always_inline unsigned long native_save_fl(void)
 {
 	unsigned long flags;
 
-	/*
-	 * "=rm" is safe here, because "pop" adjusts the stack before
-	 * it evaluates its effective address -- this is part of the
-	 * documented behavior of the "pop" instruction.
-	 */
-	asm volatile("# __raw_save_flags\n\t"
-		     "pushf ; pop %0"
-		     : "=rm" (flags)
-		     : /* no input */
-		     : "memory");
+	flags = 0;
 
 	return flags;
 }
