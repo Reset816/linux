d69ffdd632f044bc338fe158e57079672498267b
Yuan Tan <ytan089@ucr.edu>
2025-11-22 14:13:52 -0800

    Replace native_store_gdt in line 234 of arch/x86/include/asm/desc.h

--- arch/x86/include/asm/desc.h
+++ arch/x86/include/asm/desc.h
@@ -233,7 +233,7 @@ static __always_inline void native_load_idt(const struct desc_ptr *dtr)
 
 static inline void native_store_gdt(struct desc_ptr *dtr)
 {
-	asm volatile("sgdt %0":"=m" (*dtr));
+	(void)dtr;
 }
 
 static inline void store_idt(struct desc_ptr *dtr)
