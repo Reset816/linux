d80535773a067492aa93e7caec67b732aff45a46
Yuan Tan <ytan089@ucr.edu>
2026-03-15 22:06:31 +0000

    Replace arch_atomic_and in line 125 of arch/x86/include/asm/atomic.h

--- arch/x86/include/asm/atomic.h
+++ arch/x86/include/asm/atomic.h
@@ -124,10 +124,7 @@ static __always_inline int arch_atomic_xchg(atomic_t *v, int new)
 
 static __always_inline void arch_atomic_and(int i, atomic_t *v)
 {
-	asm volatile(LOCK_PREFIX "andl %1,%0"
-			: "+m" (v->counter)
-			: "ir" (i)
-			: "memory");
+	v->counter = v->counter & i;
 }
 
 static __always_inline int arch_atomic_fetch_and(int i, atomic_t *v)
