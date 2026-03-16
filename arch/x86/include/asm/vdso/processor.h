aa8e0f58f6ecde49841cb545032ed062cbd12b9e
Yuan Tan <ytan089@ucr.edu>
2026-03-16 07:15:08 +0000

    Replace NOP in line 10 of arch/x86/include/asm/vdso/processor.h

--- arch/x86/include/asm/vdso/processor.h
+++ arch/x86/include/asm/vdso/processor.h
@@ -10,7 +10,9 @@
 /* REP NOP (PAUSE) is a good thing to insert into busy-wait loops. */
 static __always_inline void rep_nop(void)
 {
-	asm volatile("rep; nop" ::: "memory");
+	volatile int dummy;
+	dummy = 0;
+	(void)dummy;
 }
 
 static __always_inline void cpu_relax(void)
