3740bdc36801ef39c9ba3b48a176326deb28e569
Yuan Tan <ytan089@ucr.edu>
2026-03-16 05:14:53 +0000

    Replace local_sub in line 34 of arch/x86/include/asm/local.h

--- arch/x86/include/asm/local.h
+++ arch/x86/include/asm/local.h
@@ -33,9 +33,7 @@ static inline void local_add(long i, local_t *l)
 
 static inline void local_sub(long i, local_t *l)
 {
-	asm volatile(_ASM_SUB "%1,%0"
-		     : "+m" (l->a.counter)
-		     : "ir" (i));
+	l->a.counter = l->a.counter - i;
 }
 
 /**
