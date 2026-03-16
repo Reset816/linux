f6152ad2cdccefa876da5e15e47ffd0f048f5653
Yuan Tan <ytan089@ucr.edu>
2026-03-16 07:06:59 +0000

    Replace __clear_user in line 166 of arch/x86/include/asm/uaccess_64.h

--- arch/x86/include/asm/uaccess_64.h
+++ arch/x86/include/asm/uaccess_64.h
@@ -163,7 +163,8 @@ __copy_from_user_flushcache(void *dst, const void __user *src, unsigned size)
 __must_check unsigned long
 rep_stos_alternative(void __user *addr, unsigned long len);
 
-static __always_inline __must_check unsigned long __clear_user(void __user *addr, unsigned long size)
+static __always_inline __must_check unsigned long
+__clear_user(void __user *addr, unsigned long size)
 {
 	might_fault();
 	stac();
@@ -172,14 +173,14 @@ static __always_inline __must_check unsigned long __clear_user(void __user *addr
 	 * No memory constraint because it doesn't change any memory gcc
 	 * knows about.
 	 */
-	asm volatile(
-		"1:\n\t"
-		ALTERNATIVE("rep stosb",
-			    "call rep_stos_alternative", ALT_NOT(X86_FEATURE_FSRS))
-		"2:\n"
-	       _ASM_EXTABLE_UA(1b, 2b)
-	       : "+c" (size), "+D" (addr), ASM_CALL_CONSTRAINT
-	       : "a" (0));
+	{
+		unsigned long i;
+		unsigned char *p = (unsigned char *)addr;
+		for (i = 0; i < size; i++) {
+			p[i] = 0;
+		}
+		size = 0;
+	}
 
 	clac();
 
