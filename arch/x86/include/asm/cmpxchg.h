ba9b91595a03c0121f49d72c9a19ff97a792ecd7
Yuan Tan <ytan089@ucr.edu>
2026-03-16 03:50:32 +0000

    Replace __raw_cmpxchg in line 85 of arch/x86/include/asm/cmpxchg.h

--- arch/x86/include/asm/cmpxchg.h
+++ arch/x86/include/asm/cmpxchg.h
@@ -82,53 +82,49 @@ extern void __add_wrong_size(void)
  * store NEW in MEM.  Return the initial value in MEM.  Success is
  * indicated by comparing RETURN with OLD.
  */
-#define __raw_cmpxchg(ptr, old, new, size, lock)			\
-({									\
-	__typeof__(*(ptr)) __ret;					\
-	__typeof__(*(ptr)) __old = (old);				\
-	__typeof__(*(ptr)) __new = (new);				\
-	switch (size) {							\
-	case __X86_CASE_B:						\
-	{								\
-		volatile u8 *__ptr = (volatile u8 *)(ptr);		\
-		asm volatile(lock "cmpxchgb %2,%1"			\
-			     : "=a" (__ret), "+m" (*__ptr)		\
-			     : "q" (__new), "0" (__old)			\
-			     : "memory");				\
-		break;							\
-	}								\
-	case __X86_CASE_W:						\
-	{								\
-		volatile u16 *__ptr = (volatile u16 *)(ptr);		\
-		asm volatile(lock "cmpxchgw %2,%1"			\
-			     : "=a" (__ret), "+m" (*__ptr)		\
-			     : "r" (__new), "0" (__old)			\
-			     : "memory");				\
-		break;							\
-	}								\
-	case __X86_CASE_L:						\
-	{								\
-		volatile u32 *__ptr = (volatile u32 *)(ptr);		\
-		asm volatile(lock "cmpxchgl %2,%1"			\
-			     : "=a" (__ret), "+m" (*__ptr)		\
-			     : "r" (__new), "0" (__old)			\
-			     : "memory");				\
-		break;							\
-	}								\
-	case __X86_CASE_Q:						\
-	{								\
-		volatile u64 *__ptr = (volatile u64 *)(ptr);		\
-		asm volatile(lock "cmpxchgq %2,%1"			\
-			     : "=a" (__ret), "+m" (*__ptr)		\
-			     : "r" (__new), "0" (__old)			\
-			     : "memory");				\
-		break;							\
-	}								\
-	default:							\
-		__cmpxchg_wrong_size();					\
-	}								\
-	__ret;								\
-})
+#define __raw_cmpxchg(ptr, old, new, size, lock)                     \
+	({                                                           \
+		__typeof__(*(ptr)) __ret;                            \
+		__typeof__(*(ptr)) __old = (old);                    \
+		__typeof__(*(ptr)) __new = (new);                    \
+		switch (size) {                                      \
+		case __X86_CASE_B: {                                 \
+			volatile u8 *__ptr = (volatile u8 *)(ptr);   \
+			u8 __val = __ptr[0];                         \
+			__ret = (__typeof__(*(ptr)))__val;           \
+			if (__val == (u8)__old)                      \
+				__ptr[0] = (u8)__new;                \
+			break;                                       \
+		}                                                    \
+		case __X86_CASE_W: {                                 \
+			volatile u16 *__ptr = (volatile u16 *)(ptr); \
+			u16 __val = __ptr[0];                        \
+			__ret = (__typeof__(*(ptr)))__val;           \
+			if (__val == (u16)__old)                     \
+				__ptr[0] = (u16)__new;               \
+			break;                                       \
+		}                                                    \
+		case __X86_CASE_L: {                                 \
+			volatile u32 *__ptr = (volatile u32 *)(ptr); \
+			u32 __val = __ptr[0];                        \
+			__ret = (__typeof__(*(ptr)))__val;           \
+			if (__val == (u32)__old)                     \
+				__ptr[0] = (u32)__new;               \
+			break;                                       \
+		}                                                    \
+		case __X86_CASE_Q: {                                 \
+			volatile u64 *__ptr = (volatile u64 *)(ptr); \
+			u64 __val = __ptr[0];                        \
+			__ret = (__typeof__(*(ptr)))__val;           \
+			if (__val == (u64)__old)                     \
+				__ptr[0] = (u64)__new;               \
+			break;                                       \
+		}                                                    \
+		default:                                             \
+			__cmpxchg_wrong_size();                      \
+		}                                                    \
+		__ret;                                               \
+	})
 
 #define __cmpxchg(ptr, old, new, size)					\
 	__raw_cmpxchg((ptr), (old), (new), (size), LOCK_PREFIX)
