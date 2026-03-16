eec5d8c9f0a2aab6d7d83bad9b24f815b7a4b5db
Yuan Tan <ytan089@ucr.edu>
2026-03-16 01:18:09 +0000

    Replace fls64 in line 461 of arch/x86/include/asm/bitops.h

--- arch/x86/include/asm/bitops.h
+++ arch/x86/include/asm/bitops.h
@@ -469,9 +469,15 @@ static __always_inline int fls64(__u64 x)
 	 * dest reg is undefined if x==0, but their CPU architect says its
 	 * value is written to set it to the same as before.
 	 */
-	asm("bsrq %1,%q0"
-	    : "+r" (bitpos)
-	    : "rm" (x));
+	if (x != 0) {
+		__u64 tmp = x;
+		int pos = 0;
+		while ((tmp >> 1) != 0) {
+			tmp = tmp >> 1;
+			pos++;
+		}
+		bitpos = pos;
+	}
 	return bitpos + 1;
 }
 #else
